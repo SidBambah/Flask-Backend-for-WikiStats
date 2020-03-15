fe2229ff508b5938c11635c5fc553f270b0d3b4a
Sidharth Bambah <sids53@gmail.com>
2020-03-15 19:29:28 -0400

    Updated requirements

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import pymongo
+import os
 app = Flask(__name__)
 
+uri = "mongodb+srv://" + os.environ.get('USERNAME') + ":" + os.environ.get('PASSWORD') + "@" + os.environ.get('HOST') + "/" + os.environ.get('DATABASE') + "?retryWrites=true&w=majority"
+
+mongoClient = pymongo.MongoClient(uri)
+db = mongoClient.get_database('wikiStats')
+daywise_changes = db.daywise_changes
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/hourlyChangesBarChart')
+def hourlyChanges():
+    try:
+        # Get query parameter
+        day = request.args.get('day')
+
+        # Look for record in database and send it back
+
+
+        return day
+    except:
+        return "Day not found"
 
 if __name__ == '__main__':
     app.run()
